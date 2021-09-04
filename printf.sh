500f7c472c3009a6626c023ed9b27f9b662a0c5c
USER <hameed.momin@gmail.com>
2021-09-04 12:41:31 +0530

    intro

--- printf.sh
+++ printf.sh
@@ -1,7 +1,11 @@
 #!bin/bash
 
+#basically i just intoduced my self
+
 echo Myself hameed
 
 echo -e"working as a DevOps engineer\nin one of the reputed company"
 
  echo -e "yes please lemme knw\nwhat can i do for u"
+
+ echo Im fine you can ask me anything
