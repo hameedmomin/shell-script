bd39da6669be183eb5d9c59ab26b77e509c668f4
USER <hameed.momin@gmail.com>
2021-09-04 12:47:51 +0530

    just for understanding

--- printf.sh
+++ printf.sh
@@ -9,3 +9,13 @@ echo -e"working as a DevOps engineer\nin one of the reputed company"
  echo -e "yes please lemme knw\nwhat can i do for u"
 
  echo Im fine you can ask me anything
+
+ echo I like my name to be in colours
+
+ echo -e "\tStory, a narrative (an account of imaginary or real people and events)
+          Short story, a piece of prose fiction that typically can be read in one sitting
+          Story, or storey, a floor or level of a building
+          News story, an event or topic reported by a news organization"
+
+echo -e "\e[31mMOMIN"
+
