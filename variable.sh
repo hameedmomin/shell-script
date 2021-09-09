14ae7afce550d0bbcf2c46f38d0e7aa03fbc66f5
USER <hameed.momin@gmail.com>
2021-09-09 19:56:22 +0530

    added

--- variable.sh
+++ variable.sh
@@ -20,34 +20,4 @@ NAME=HAMEEDUDDIN
 
 echo myself $NAME
 
- # Author : Momin
- echo -n "Enter your DOB [ddmmyyyy] : "
- read n
- d=`echo $n | cut -c1-2`
- m=`echo $n | cut -c3-4`
- y=`echo $n | cut -c5-8`
- yy=`date "+%Y"`
- mm=`date "+%m"`
- dd=`date "+%d"`
- if [ $y -le $yy ]
- then
- yyy=`expr $yy - $y`
- mmm=`expr $mm - $m`
- #ddd=`expr $dd - $d`
- if [ $m -gt $mm ]
- then
- yyy=`expr $yyy - 1`
- mmm=`expr $mmm + 12`
- fi
- if [ $d -gt $dd ]
- then
- ddd=`expr $d - $dd`
- ddd=`expr 31 - $ddd`
- else
- ddd=`expr $dd - $d`
- fi
- fi
- echo "Your age : $yyy years $mmm months $ddd days"
-
-
- echo -e "\e[1m35mHAMEEDUDDIN \e[1m32mmiss u \0m"
+ echo -e'\[32mgood thing'
