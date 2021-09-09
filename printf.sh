d96efb52293ede6e8f91bd74631559775ff91453
USER <hameed.momin@gmail.com>
2021-09-09 20:08:20 +0530

    hi

--- printf.sh
+++ printf.sh
@@ -29,3 +29,4 @@ echo -e "\e[33mHAMEEDUDDIN\e[0mGood boy"
 echo -e "\e[35meverytime learn the code easily \e[31mwe should b consious. \e[0m"
 
 echo -e '\e[33mgood \e[0m'
+echo -e '\e[33mgood \e[0m'
