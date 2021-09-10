f7eafa6d2415835222d9d874350fbd3a93781d1e
USER <hameed.momin@gmail.com>
2021-09-10 10:43:43 +0530

    discription of input before excutions

--- read.sh
+++ read.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+## the read command is only used when we given inputs before excutions
+
+## -p is promt and read is command
+
  read -p 'enter your Name : '
 
  echo "your Name = $name"
