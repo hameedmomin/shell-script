127bd099da72320900c565a67bc12a31e5ef19e1
USER <hameed.momin@gmail.com>
2021-09-18 18:27:28 +0530

    if

--- if.sh
+++ if.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## read -p it is the input which is given by user while excution
 
-read -p 'Enter username : '
+read -p 'Enter username : ' username
 
 if [ "$username" == "root" ] ; then
   echo -e "hey user is $username is a 'Hameed the king' "
