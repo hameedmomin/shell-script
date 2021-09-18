2d35e0e87e582abd7bb7af3fd97925e50b12f45a
USER <hameed.momin@gmail.com>
2021-09-18 18:21:38 +0530

    if

--- if.sh
+++ if.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 ## read -p it is the input which is given by user while excution
 
-read -p 'Enter username : ' username
+read -p 'Enter username : '
 
 if [ "$username" == "root" ] ; then
   echo -e "hey user is $username is a 'Hameed the king' "
 else
   echo -e "hey user is $username is normal "
+fi
+
+read -p 'File exist'
+
+if [ -f "$filename" ] ; then
+
+  echo "file exist"
+else
+  echo "file doesnot exist"
 fi
