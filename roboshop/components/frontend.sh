6eb5c43ab721fa99ed094244ee7d3a956fec1472
USER <hameed.momin@gmail.com>
2021-09-18 19:36:42 +0530

    frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,8 +25,8 @@ HEAD "Extracting file \t"
 unzip -d /usr/share/nginx/html /tmp/frontend.zip  &>>/tmp/roboshop.log
 mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
 mv  /usr/share/nginx/html/static/* /usr/share/nginx/html/.  &>>/tmp/roboshop.log
-# rm -rf frontend-master README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
 STAT $?
 
+HEAD "Upadate Nginx \t"
+mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT &?
