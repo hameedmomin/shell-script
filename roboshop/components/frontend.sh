6573555e2b6bf3cba3d88476b1e280a2053b6ec8
USER <hameed.momin@gmail.com>
2021-09-18 19:02:09 +0530

    frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,11 +9,11 @@ yum install nginx -y  &>>/tmp/roboshop.log
 STAT $?
 
 HEAD "Start Nginx"
-systectl start nginx
+systemctl start nginx
 systemctl enable nginx
 STAT $?
 
-HEAD "Downloading "
+HEAD "Downloading \t\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
@@ -21,7 +21,7 @@ HEAD "Deleting old HTML Docs"
 rm -rf /usr/share/nginx/html/*
 STAT $?
 
-HEAD "Extracting file"
+HEAD "Extracting file \t"
 unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
 STAT $?
 
