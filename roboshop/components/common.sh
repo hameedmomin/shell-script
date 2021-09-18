20727621c22494bdc6ffcd7f71f930325c07edb4
USER <hameed.momin@gmail.com>
2021-09-18 11:06:54 +0530

    common

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,10 +1,10 @@
 HEAD() {
-  echo -n -e "\e[1m $1 \e[0m \t\t ..."
+    echo -n -e "\e[1m $1 \e[0m \t\t ..."
 }
 STAT() {
   if [ $1 -eq 0 ]; then
-    echo -e "\e[1;32mDone"
+    echo -e "\e[1;32mDone\e[0m"
   else
-    echo -e "\e[1;31mFail"
+    echo -e "\e[1;31mFail\e[0m"
   fi
 }
