d9fb96b1dd5bc7ad81a12adae63582bb536cb77d
USER <hameed.momin@gmail.com>
2021-09-18 08:28:58 +0530

    frontend

---
+++ roboshop/components/common.sh
@@ -0,0 +1,3 @@
+HEAD() {
+  echo -n -e "\[1m $1m \e[0m \t\t ..."
+}
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -n "Installing Nginx ..."
+HEAD -n "Installing Nginx ..."
 yum install nginx -y &>>/tmp/roboshop.log
-echo -e "\e[32mDone\e[0m hello"
+echo -e "\e[32mDone\e[0m"
