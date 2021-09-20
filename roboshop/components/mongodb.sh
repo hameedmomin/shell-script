0cebed8d51afc44521e1e036a057d7863ddc5bfe
USER <hameed.momin@gmail.com>
2021-09-20 07:33:39 +0530

    Mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -31,3 +31,15 @@ HEAD "Downloading Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 STAT $?
 
+HEAD "unzipping files"
+cd /tmp
+unzip mongodb.zip &>>/tmp/robosho.log
+
+STAT $?
+
+HEAD "Remaining file"
+cd mongodb-main
+
+mongo < catalogue.js && mongo < users.js &>>/tmp/robosho.log
+STAT &?
+
