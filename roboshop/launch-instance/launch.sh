a4814e30b8ff33570989768771ca7a6caf0291ea
USER <hameed.momin@gmail.com>
2021-09-22 16:15:07 +0530

    launch instance

--- roboshop/launch-instance/launch.sh
+++ roboshop/launch-instance/launch.sh
@@ -10,4 +10,4 @@ fi
 LID=lt-0b970bef7d9f13f52
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
