c062140b948596711c5a27a7d4e9372367002671
USER <hameed.momin@gmail.com>
2021-09-22 18:43:36 +0530

    launch instance

--- roboshop/launch-instance/launch.sh
+++ roboshop/launch-instance/launch.sh
@@ -10,4 +10,12 @@ fi
 LID=lt-0b970bef7d9f13f52
 LVER=1
 
+#validate if instance is already their
+
+INSTANCE_STATE=$(aws ec2 describe-instance --filters "Name=tag:Name,Values=${COMPONENT}" | jq.Reservations[].Instance[].State.Name | xargs -n1)
+if [ "${INSTANCE_STATE}" = "running" ]; then
+  echo "Instance already exist"
+  exit 0
+fi
+
 aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
