d68f586ecacd80761aea36093e6302a11e35faed
USER <hameed.momin@gmail.com>
2021-09-23 15:56:00 +0530

    launch

--- roboshop/launch/launch.sh
+++ roboshop/launch/launch.sh
@@ -16,14 +16,14 @@ LVER=1
 
 INSTANCE_CREATE(){
 
-INSTANCE_STATE=$(aws ec2 describe-instance --filters "Name=tag:Name,Values=${COMPONENT}" | jq.Reservations[].Instance[].State.Name | xargs -n1)
+  INSTANCE_STATE=$(aws ec2 describe-instance --filters "Name=tag:Name,Values=${COMPONENT}" | jq.Reservations[].Instance[].State.Name | xargs -n1)
 
-echo -n Instance ${COMPONENT} created - IPADDRESS IS
+  echo -n Instance ${COMPONENT} created - IPADDRESS IS
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
-sleep 30
+  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
+  sleep 30
 
-DNS_UPDATE
+  DNS_UPDATE
 if [ "${INSTANCE_STATE}" = "running" ]; then
   echo "Instance already exist"
   DNS_UPDATE
@@ -34,5 +34,5 @@ if [ "${INSTANCE_STATE}" = "stopped" ]; then
   echo "${COMPONENT} Instance already exit"
   retrun 0
 fi
-  }
+  "}
 
