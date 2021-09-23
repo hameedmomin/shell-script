7cf8d93ec03acbb45aa1457bd6529b34f0333165
USER <hameed.momin@gmail.com>
2021-09-23 08:22:50 +0530

    launch

--- roboshop/launch/launch.sh
+++ roboshop/launch/launch.sh
@@ -13,17 +13,10 @@ LID=lt-0b970bef7d9f13f52
 LVER=1
 
 #validate if instance is already their
-
-DNS_UPDATE(){
-  PRIVATEIP=$(aws ec2 describe-instance --filters "Name=tag:Name,Values=${COMPONENT}" | jq.Reservations[].Instance[].PrivateIpAddress | xargs -n1)
-  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z07567143CWQBG84ZKWAY --change-batch file:///tmp/record.json |jq
-
-}
-
 INSTANCE_CREATE(){
 
   INSTANCE_STATE=$(aws ec2 describe-instance --filters "Name=tag:Name,Values=${COMPONENT}" | jq.Reservations[].Instance[].State.Name | xargs -n1)
+
 if [ "${INSTANCE_STATE}" = "running" ]; then
   echo "Instance already exist"
   DNS_UPDATE
@@ -39,4 +32,10 @@ echo -n Instance ${COMPONENT} created - IPADDRESS IS
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
   sleep 10
   DNS_UPDATE
+}
+
+DNS_UPDATE(){
+  PRIVATEIP=$(aws ec2 describe-instance --filters "Name=tag:Name,Values=${COMPONENT}" | jq.Reservations[].Instance[].PrivateIpAddress | xargs -n1)
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z07567143CWQBG84ZKWAY --change-batch file:///tmp/record.json |jq
 }
