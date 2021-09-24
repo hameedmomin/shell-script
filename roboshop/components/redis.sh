1bc34c886e3a0a60db27532da1342730aafa23b5
USER <hameed.momin@gmail.com>
2021-09-24 16:28:43 +0530

    launch

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -3,3 +3,18 @@
 source components/common.sh
 rm -f tmp/robosho.log
 set-hostname redis
+
+HEAD "Setup repo "
+yum install epel-release yum-utils install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/robosho.log && yum-config-manager --enable remi &>>/tmp/robosho.log
+STAT $?
+
+HEAD "Install Redis"
+yum install redis -y &>>/tmp/robosho.log
+STAT $?
+
+HEAD "Update IP address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis..conf
+
+HEAD"start Redis Service"
+systemctl enable redis &>>/tmp/robosho.log && systemctl restart redis &>>/tmp/robosho.log
+STAT $?
