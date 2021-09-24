374e89972eddaba2f64510c2cd20c7704b132c86
USER <hameed.momin@gmail.com>
2021-09-24 22:20:08 +0530

    launch

--- roboshop/launch/launch.sh
+++ roboshop/launch/launch.sh
@@ -14,11 +14,12 @@ LVER=1
 
 ## Validating the instance is already there
 
-DNS_UPDATE()
+DNS_UPDATE() {
   PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instance[].PrivateIpAddress | xargs -n1)
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z0030742HZD6U31Q306U --change-batch file:///tmp/record.json | jq
 
+}
 
 INSTANCE_CREATE() {
   INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instance[].State.Name | xargs -n1)
