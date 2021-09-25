719653ed3dd7071b72313036599da5c359dcb3e3
USER <hameed.momin@gmail.com>
2021-09-25 07:12:24 +0530

    launch

--- roboshop/launch/launch.sh
+++ roboshop/launch/launch.sh
@@ -16,25 +16,22 @@ LVER=1
 
 DNS_UPDATE() {
   PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instance[].PrivateIpAddress | xargs -n1)
-  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z0030742HZD6U31Q306U --change-batch file:///tmp/record.json | jq
-
 }
 
-INSTANCE_CREATE() {
-  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instance[].State.Name | xargs -n1)
-  if [ "${INSTANCE_STATE}" = "running" ]; then
-    echo "${COMPONENT} Instance already exist"
-    DNS_UPDATE
-    return 0
-  fi
+INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instance[].State.Name | xargs -n1)
 
-  if [ "${INSTANCE_STATE}" = "stopped" ]; then
-    echo "${COMPONENT} Instance already exit"
-    return 0
-  fi
+if [ "${INSTANCE_STATE}" = "running" ]; then
+  echo "${COMPONENT} Instance already exist"
+  DNS_UPDATE
+  exit 0
+fi
 
-  echo -n Instance ${COMPONENT} created - IPADDRESS is
-  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
-  sleep 30
-}
+if [ "${INSTANCE_STATE}" = "stopped" ]; then
+  echo "Instance already exit"
+  exit 0
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" | jq
+sleep 30
