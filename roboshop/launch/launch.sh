0508a900dc49f18e216e36ae20a41f18fde138b3
USER <hameed.momin@gmail.com>
2021-09-26 10:16:38 +0530

    launch

--- roboshop/launch/launch.sh
+++ roboshop/launch/launch.sh
@@ -29,8 +29,3 @@ INSTANCE_CREATE() {
 
 }
 aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
-sleep 30
-
-PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instance[].PrivateIpAddress | xargs -n1)
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z05483541JD3IOXLSP16I --change-batch file:///tmp/record.json
