8df48b3aefd50649de660d4d458113af2c4c8dcf
USER <hameed.momin@gmail.com>
2021-09-26 11:08:07 +0530

    launch

--- roboshop/launch/launch.sh
+++ roboshop/launch/launch.sh
@@ -14,18 +14,16 @@ LVER=2
 
 ## Validating the instance is already there
 
-INSTANCE_CREATE() {
-  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instance[].State.Name | xargs -n1)
+INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs -n1)
 
-  if [ "${INSTANCE_STATE}" = "Running" ]; then
-    echo "Instance already exists"
-    exit 0
-  fi
+if [ "${INSTANCE_STATE}" = "running" ]; then
+  echo "Instance already exists"
+  exit 0
+fi
 
-  if [ "${INSTANCE_STATE}" = "Stopped" ]; then
-    echo "Instance already exists!!"
-    exit 0
-  fi
+if [ "${INSTANCE_STATE}" = "stopped" ]; then
+  echo "Instance already exists!!"
+  exit 0
+fi
 
-}
 aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
