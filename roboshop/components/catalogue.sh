08e781aa724d6ccd4b4537c49983f5c4a12e59a6
USER <hameed.momin@gmail.com>
2021-09-27 18:22:53 +0530

    catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -25,7 +25,7 @@ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/cat
 STAT $?
 
 HEAD "Extracting Flies \t"
-cd /home/roboshop && rm -f catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue &>>/tmp/roboshop.log
+cd /home/roboshop && rm -f /tmp/catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue &>>/tmp/roboshop.log
 STAT $?
 
 HEAD "Installing Nodejs Files "
