c0e76a6c8cca997899aa037e1f3bded2ce80f8e3
USER <hameed.momin@gmail.com>
2021-09-27 21:43:12 +0530

    updating

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -2,5 +2,18 @@
 
 source components/common.sh
 rm -f tmp/roboshop.log
+HEAD "Setup MYSQL service"
+echo '[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
+enabled=1
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+STAT $?
 
-NODEJS "mysql"
+HEAD "Install MySQL Server"
+yum remove mariadb-libs -y &>>/tmp/roboshop.log && yum install mysql-community-server -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Start MySQL Service"
+systemctl enable mysqld &>>/tmp/roboshop.log && systemctl start mysqld &>>/tmp/roboshop.log
+STAT $?
