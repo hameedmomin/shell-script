99f457ba443ee5e1e5ae28298febde9aff7c09d4
USER <hameed.momin@gmail.com>
2021-09-27 19:05:13 +0530

    redis

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 rm -f tmp/robosho.log
-set-hostname redis
+
 
 HEAD "Setup repo "
 yum install epel-release yum-utils install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/robosho.log && yum-config-manager --enable remi &>>/tmp/robosho.log
