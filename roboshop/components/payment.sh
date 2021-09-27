6a7273c2fc5d52e1b89996f3c364ea3b7538a3be
USER <hameed.momin@gmail.com>
2021-09-27 22:31:56 +0530

    updating

---
+++ roboshop/components/payment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source components/common.sh
+rm -f /tmp/roboshop.log
+
+PYTHON3 "payment"
--- roboshop/launch/launch.sh
+++ roboshop/launch/launch.sh
@@ -26,12 +26,12 @@ INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${CO
 if [ "${INSTANCE_STATE}" = "running" ]; then
   echo "Instance already exists"
   DNS_UPDATE
-  exit 0
+  return 0
 fi
 
 if [ "${INSTANCE_STATE}" = "stopped" ]; then
   echo "Instance already exists!!"
-  exit 0
+  return 0
 fi
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
