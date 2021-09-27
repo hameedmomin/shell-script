c781b88130d6511a8917c70784e2c3cac14ae2d6
USER <hameed.momin@gmail.com>
2021-09-27 23:24:33 +0530

    updating

--- roboshop/launch/launch.sh
+++ roboshop/launch/launch.sh
@@ -20,21 +20,32 @@ DNS_UPDATE() {
   aws route53 change-resource-record-sets --hosted-zone-id Z05483541JD3IOXLSP16I --change-batch file:///tmp/record.json | jq
 }
 
+INSTANCE_CREATE() {
+  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs -n1)
+  if [ "${INSTANCE_STATE}" = "running" ]; then
+    echo "${COMPONENT} Instance already exists"
+    DNS_UPDATE
+    return 0
+  fi
 
-INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs -n1)
+  if [ "${INSTANCE_STATE}" = "stopped" ]; then
+    echo "${COMPONENT} Instance already exists!!"
+    return 0
+  fi
 
-if [ "${INSTANCE_STATE}" = "running" ]; then
-  echo "Instance already exists"
+  echo -n Instance ${COMPONENT} created - IPADDRESS is
+  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
+  sleep 30
   DNS_UPDATE
-  exit 0
+
+}
+
+if [ "${1}" == "all" ]; then
+  for component in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment ; do
+    COMPONENT=$component
+    INSTANCE_CREATE
+  done
+else
+  COMPONENT=$1
+  INSTANCE_CREATE
 fi
-
-if [ "${INSTANCE_STATE}" = "stopped" ]; then
-  echo "Instance already exists!!"
-  exit 0
-fi
-
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
-sleep 30
-DNS_UPDATE
-
