db204e1954707b9b5b52b1f82baf464accda9bf4
USER <hameed.momin@gmail.com>
2021-09-29 19:30:09 +0530

    updating

---
+++ loops.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#there are two type of loops "while" and "for" loops
+
+# Syntax
+# while [ expression ] ; do
+# commands
+#done
+
+i=10
+while [ $i -gt 0 ]; do
+  echo $i
+  i=$(($i-1))
+done
+
+# for var in values ; do
+# commands
+# done
+
+for fruit in apple banana orange ; do
+  echo Fruit Name = $fruit
+  sleep 1
+done
